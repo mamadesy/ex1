9e5eb51518efb1bcd2ab121f9606cdeb41dceb3b
Mohammad Esmaeili <79360273+mamadesy@users.noreply.github.com>
2021-04-15 13:32:10 +0430

    Add files via upload

---
+++ Rand2.py
@@ -0,0 +1,15 @@
+import  random
+import os
+os.system('cls')
+
+
+def Rand2D(a, b):
+    n = random.randint(a+1, b+1)
+    while n % 2 != 0:
+        n = random.randint(a, b)
+    return n
+
+
+
+print(Rand2D(2, 6))
+
